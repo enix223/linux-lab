02105d9176a348e2dfe14c2ef94f46da18805ab6
Enix Yu <enix223@163.com>
2025-10-28 15:01:37 +0000

    feat: unlink demo

--- src/fileio/file_link.cpp
+++ src/fileio/file_link.cpp
@@ -18,21 +18,30 @@ public:
 
   int run() override
   {
-    const auto filename = "temp.txt";
+    const auto filename1 = "temp1.txt";
+    const auto filename2 = "temp2.txt";
     auto &logger = cl::Logger::getInstance();
-    int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
-    if (fd < 0)
+    int fd1 = open(filename1, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (fd1 < 0)
     {
       logger.error(std::format("failed to open file: {}", std::strerror(errno)));
       return -1;
     }
-    if (unlink(filename) < 0)
+    int fd2 = open(filename2, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (fd1 < 0)
+    {
+      logger.error(std::format("failed to open file: {}", std::strerror(errno)));
+      return -1;
+    }
+
+    if (unlink(filename1) < 0)
     {
       logger.error(std::format("failed to unlink file: {}", std::strerror(errno)));
       return -2;
     }
+
     std::string msg = "hello world";
-    if (write(fd, msg.c_str(), msg.size()) < 0)
+    if (write(fd1, msg.c_str(), msg.size()) < 0)
     {
       logger.error(std::format("failed to unlink file: {}", std::strerror(errno)));
       return -3;
@@ -41,7 +50,14 @@ public:
     std::cout << "press any key to continue";
     getchar();
 
-    close(fd);
+    if (unlink(filename2) < 0)
+    {
+      logger.error(std::format("failed to unlink file: {}", std::strerror(errno)));
+      return -2;
+    }
+
+    close(fd1);
+    close(fd2);
 
     return 0;
   }
